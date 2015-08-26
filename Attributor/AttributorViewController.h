574f7860bd2e85b1e92fd686d565f656ddc4448d
John Lu <luj1991@gmail.com>
2015-08-26 08:10:01 -0700

    add outline, unoutline, select color features

---
+++ Attributor/AttributorViewController.h
@@ -0,0 +1,13 @@
+//
+//  AttributorViewController.h
+//  Attributor
+//
+//  Created by John Lu on 8/26/15.
+//  Copyright (c) 2015 voyager. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AttributorViewController : UIViewController
+
+@end
