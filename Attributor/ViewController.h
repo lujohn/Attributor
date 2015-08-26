de11e474ebe6ac17f48af888d830dc0315889e7a
John Lu <luj1991@gmail.com>
2015-08-26 06:07:16 -0700

    initial commit

---
+++ Attributor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Attributor
+//
+//  Created by John Lu on 8/26/15.
+//  Copyright (c) 2015 voyager. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Attributor/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Attributor
+//
+//  Created by John Lu on 8/26/15.
+//  Copyright (c) 2015 voyager. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+   @autoreleasepool {
+       return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+   }
+}
